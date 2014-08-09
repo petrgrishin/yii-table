351462a09431e8078e123c792dbacf7617ed6966
Petr Grishin <petr.grishin@grishini.ru>
2014-08-09 23:57:36 +0400

    Реализация класса колонок таблицы

--- src/Column.php
+++ src/Column.php
@@ -7,6 +7,41 @@ namespace PetrGrishin\Table;
 
 
 class Column {
+    /** @var string */
+    protected $name;
+    /** @var boolean */
+    protected $sortable;
 
+    /**
+     * @return string
+     */
+    public function getName() {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return $this
+     */
+    public function setName($name) {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isSortable() {
+        return $this->sortable;
+    }
+
+    /**
+     * @param boolean $sortable
+     * @return $this
+     */
+    public function setSortable($sortable = true) {
+        $this->sortable = $sortable;
+        return $this;
+    }
 }
  
